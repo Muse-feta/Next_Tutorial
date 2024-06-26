600c028f951aaaf830a27661b0258099e811d9d8
Muse-feta <fetamuse@gmail.com>
2024-06-26 23:22:05 +0300

    modify user table

--- auth_next/dbconfig/dbconfig.ts
+++ auth_next/dbconfig/dbconfig.ts
@@ -1,20 +1,36 @@
-import mongoose from "mongoose";
+import mysql, { Connection } from "mysql2/promise";
+import fs from "fs";
+import path from "path";
 
-export const connectDB = async () => {
-    try {
-        mongoose.connect(process.env.MONGO_URI!)
-        const connection = mongoose.connection
 
-        connection.on('open', () => {
-            console.log('MongoDB connection established successfully')
-        })
 
-        connection.on('error', (error) => {
-            console.log(error)
-            process.exit()
-        })
+const dbConfig = {
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, // Your MySQL database name
+};
 
-    } catch (error) {
-        console.log(error)
-    }
+export async function connect(): Promise<Connection> {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    await createTables(connection);
+    return connection;
+  } catch (error) {
+    console.error("Error connecting to MySQL:", error);
+    throw new Error("Could not connect to database");
+  }
+}
+
+async function createTables(connection: Connection): Promise<void> {
+  try {
+    
+    const schemaPath = path.join(__dirname, "../schema/schema.sql");
+    const sql = fs.readFileSync(schemaPath, "utf8");
+    await connection.query(sql);
+    console.log("Tables created successfully");
+  } catch (error) {
+    console.error("Error creating tables:", error);
+    throw new Error("Could not create tables");
+  }
 }
